80e498c69e54b9e507f0670149fa173a56282e20
meriemnour <meriemnour.ghozzi@esprit.tn>
2022-12-08 11:25:27 +0100

    update css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <header>
-        <h1 style={headingStyle}>{title}</h1>
+        <h1 >{title}</h1>
     </header>
   )
 }
@@ -15,8 +15,9 @@ Header.defaultProps={
     title: PropTypes.string.isRequired,
 }*/
 
-const headingStyle={
+//CSS in JS
+/*const headingStyle={
   color:'red',
   backgroundColor:'black'
-}
+}*/
 export default Header
